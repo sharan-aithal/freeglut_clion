4e3adc2b935ba39475a6610e8d7573a24d589ae1
Sharan Aithal <sharanaithal0@yahoo.com>
2021-05-04 18:24:18 +0530

    update source program to a demo OpenGL program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include "GL/glut.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void displayMe(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.5,0.8,0.2);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.5, 0.0, 0.5);
+    glVertex3f(0.5, 0.0, 0.0);
+    glVertex3f(0.0, 0.5, 0.0);
+    glVertex3f(0.0, 0.0, 0.5);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello world!");
+    glutDisplayFunc(displayMe);
+    glutMainLoop();
     return 0;
 }
